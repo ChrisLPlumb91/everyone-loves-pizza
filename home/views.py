7b9522f268cbc0755e48988d5bc5e0f735ea0445
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-05-20 19:21:41 +0000

    Add jumbotron and about section to home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from menu.models import MenuItem
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the index page """
+    template = 'home/index.html'
+    
+    featured_pizzas = []
 
-    return render(request, 'home/index.html')
+    for number in range(1, 5):
+        featured_pizzas.append(get_object_or_404(MenuItem, pk=number))
+
+    context = {
+        'featured_pizzas': featured_pizzas,
+    }
+
+    return render(request, template, context)
--- menu/models.py
+++ menu/models.py
@@ -25,7 +25,6 @@ class MenuItem(models.Model):
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
