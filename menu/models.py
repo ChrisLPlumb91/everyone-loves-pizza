e4ede5a3d0e4bbf6a757cc59eaaa95e5c383ba12
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-05-20 22:10:36 +0000

    Add about and featured sections to home page, and add footer to base.html

--- menu/models.py
+++ menu/models.py
@@ -22,9 +22,11 @@ class MenuItem(models.Model):
                                  blank=True,
                                  on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True, default=0)
+    calories = models.CharField(max_length=25)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
