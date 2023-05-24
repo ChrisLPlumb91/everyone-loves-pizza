8b61d6d97147cb11774cf0a5d207798161f2ff5e
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-05-24 00:04:59 +0000

    Add menu item detail page

--- menu/models.py
+++ menu/models.py
@@ -29,6 +29,8 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    full_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    full_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
