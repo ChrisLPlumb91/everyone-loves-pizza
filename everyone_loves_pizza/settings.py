26d4343a1b9ee45178724b6e3c59db8b179068c5
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2023-06-22 00:17:15 +0000

    Add secret key and development environmental variables and assign them in settings.py

--- everyone_loves_pizza/settings.py
+++ everyone_loves_pizza/settings.py
@@ -16,17 +16,18 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*5#^3o*2^hv(#nkg0#g!h%&cn+&mdjwu&g40tmeq=_vs$r&hj'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEVELOPMENT in os.environ
 
 ALLOWED_HOSTS = ['clp1991-everyone-loves-pizza-a520c76d55de.herokuapp.com', 'localhost']
 
